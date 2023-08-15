884760bec7d3824b603596c01d25cb690c2c3b8f
MaraMincho <103064352+MaraMincho@users.noreply.github.com>
2023-08-15 23:42:05 +0900

    기능 추가 : CollectionView 기능 추가
    기능 추가 : CollectionView 기능 추가

---
+++ CollectionViewExample/CollectionViewExample/TestCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  TestCollectionViewCell.swift
+//  CollectionViewExample
+//
+//  Created by MaraMincho on 2023/08/15.
+//
+
+import UIKit
+
+class TestCollectionViewCell: UICollectionViewCell {
+    
+}
