c6a8aeb1ecbf7559313b44f21bb4d98fb4697b4e
MaraMincho <seemaster300@naver.com>
2024-01-03 15:02:18 +0900

    feat: 프로젝트 생성

---
+++ KingFisherExample/KingFisherExample/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  KingFisherExample
+//
+//  Created by MaraMincho on 1/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .red
+  }
+}
+
