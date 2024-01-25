a6ee8fd6bbd5d90852c3068b13c3849738df0f49
MaraMincho <103064352+MaraMincho@users.noreply.github.com>
2024-01-25 15:44:16 +0900

    프로젝트: KingFisher추가

--- QueenFisher/QueenFisher/ViewController.swift
+++ QueenFisher/QueenFisher/ViewController.swift
@@ -6,11 +6,27 @@
 //
 
 import UIKit
+import Kingfisher
 
 class ViewController: UIViewController {
   
+  let someImageView: UIImageView = {
+    let imageView = UIImageView()
+    
+    imageView.translatesAutoresizingMaskIntoConstraints = false
+    return imageView
+  }()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
+    setupStyle()
+  }
+  
+  
+  func setupStyle() {
+    view.backgroundColor = .white
+    
+    view.addSubview(someImageView)
     
   }
 }
