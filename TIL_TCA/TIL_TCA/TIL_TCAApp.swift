a1e51234840bd52dd8aa95123e5865d59eee66e6
MaraMincho <103064352+MaraMincho@users.noreply.github.com>
2024-05-05 12:14:51 +0900

    기능추가: TsakModifier 기능 수정

--- TIL_TCA/TIL_TCA/TIL_TCAApp.swift
+++ TIL_TCA/TIL_TCA/TIL_TCAApp.swift
@@ -13,11 +13,7 @@ struct TIL_TCAApp: App {
   var body: some Scene {
     WindowGroup {
       
-      LongLivingEffectsView(
-        store: Store(initialState: LongLivingEffects.State()) {
-          LongLivingEffects()
-        }
-      )
+      TaskModifierView()
       
     }
   }
--- TIL_TCA/TIL_TCA/TestContents/TaskModifier.swift
+++ TIL_TCA/TIL_TCA/TestContents/TaskModifier.swift
@@ -15,36 +15,41 @@ struct Message: Decodable, Identifiable {
 }
 
 struct TaskModifierView: View {
-    @State private var messages = [Message]()
-
-    var body: some View {
-        NavigationView {
-            List(messages) { message in
-                VStack(alignment: .leading) {
-                    Text(message.from)
-                        .font(.headline)
-
-                    Text(message.text)
-                }
-            }
-            .navigationTitle("Inbox")
-            .task {
-                await loadMessages()
-            }
+  @State private var messages = [Message]()
+  
+  var body: some View {
+    NavigationView {
+      List(messages) { message in
+        VStack(alignment: .leading) {
+          Text(message.from)
+            .font(.headline)
+          
+          Text(message.text)
         }
+      }
+      .navigationTitle("Inbox")
+      .onAppear{
+        print("View is appeard")
+      }
+      .task {
+        print("Task was Started")
+        await loadMessages()
+        print("Task was end")
+      }
     }
-
-    func loadMessages() async {
-        do {
-            let url = URL(string: "https://hws.dev/messages.json")!
-            let (data, _) = try await URLSession.shared.data(from: url)
-            messages = try JSONDecoder().decode([Message].self, from: data)
-        } catch {
-            messages = [
-                Message(id: 0, from: "Failed to load inbox.", text: "Please try again later.")
-            ]
-        }
+  }
+  
+  func loadMessages() async {
+    do {
+      let url = URL(string: "https://hws.dev/messages.json")!
+      let (data, _) = try await URLSession.shared.data(from: url)
+      messages = try JSONDecoder().decode([Message].self, from: data)
+    } catch {
+      messages = [
+        Message(id: 0, from: "Failed to load inbox.", text: "Please try again later.")
+      ]
     }
+  }
 }
 
 #Preview {
