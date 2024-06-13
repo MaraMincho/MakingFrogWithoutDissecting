53f9cea8da8c3180030360a9058036103b5ffaca
MaraMincho <103064352+MaraMincho@users.noreply.github.com>
2024-06-13 23:35:52 +0900

    feat: cpp 커밋

--- TestCpp/TestCpp/main.cpp
+++ TestCpp/TestCpp/main.cpp
@@ -1,37 +1,19 @@
 #include <iostream>
-#include <string> 
-#include <map>
-#include <random>
+#include <cstring>
+
 using namespace std;
 
 int main() {
-  vector<int> curStack;
-  for(int i = 0; i < 5; i ++) {
-    int curRanVal = random() % 10;
-    int curVal = 0;
-    for(int i = 0; i < curRanVal; i ++)
-      curVal = random() % 100;
-    cout << curVal << " ";
-    curStack.push_back(curVal);
-  }
-  sort(curStack.begin(), curStack.end());
-  
-  int targetVal = 50;
-  
-  for_each(curStack.begin(), curStack.end(), [targetVal](int x) -> void {
-    cout << "targetVal(" << x << ") is " << ((x < 50) ? "smaller" : "bigger") << " then 50" << endl;
-  });
-  
-  int sum = 0;
-  
-  for_each(curStack.begin(), curStack.end(), [&sum](auto x) -> void {
-    sum += x;
-    return ;
-  });
-  
-  cout << "썸은 ? : " <<sum << endl;
-  
-  for(auto cur: curStack) {
-    cout << cur << " ";
+  char cmd[80];
+  cout << "cin.get(char*, int)로 문자열을 읽습니다." << endl;
+  while(true) {
+    cout << "종료하려면 exit을 입력하세요 >> ";
+    cin.get(cmd, 80); // 79개까지의 영어 문자 읽음.
+    if(strcmp(cmd, "exit") == 0) {
+      cout << "프로그램을 종료합니다....";
+      return 0;
+    }
+    else
+      cin.ignore(1); // 버퍼에 남아 있는 <Enter> 키 ('\n') 제거
   }
 }
