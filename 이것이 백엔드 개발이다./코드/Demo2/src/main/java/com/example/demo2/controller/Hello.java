83445999fbf024f5788e93c5eecf581acb432f21
MaraMincho <103064352+MaraMincho@users.noreply.github.com>
2025-01-10 14:57:37 +0900

    feat: add mavn demo3

---
+++ 이것이 백엔드 개발이다./코드/Demo2/src/main/java/com/example/demo2/controller/Hello.java
@@ -0,0 +1,12 @@
+package com.example.demo2.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello world";
+    }
+}
